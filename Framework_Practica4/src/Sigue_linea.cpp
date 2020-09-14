facbd093406a67267fdc654e41080c0f61886c4b
Xabi Garde <xabier.gardeazabal@ehu.eus>
2020-09-14 16:44:27 +0200

    Añadir y renombrar ficheros

---
+++ Framework_Practica4/src/Sigue_linea.cpp
@@ -0,0 +1,26 @@
+/** 
+*	Versión del algoritmo sigue líneas para 3 estados usando 1 sensor de Cliff
+*/
+
+#include "ControlRobot.h"
+
+using namespace std;
+
+int main(int argc, char * argv[])
+{
+	
+	ControlRobot robot;
+	
+	robot.inicializacion();
+	
+	while(!robot.condicionSalida()){
+		robot.leerSensores();
+		robot.logicaEstados();
+		robot.moverActuadores();
+		robot.imprimirInfo();
+
+	}
+
+	robot.finalizacion();
+	return 0;
+}
